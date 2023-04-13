8937ca014fc97a960213251a9d1128b1ac906aed
JxAllen <icerleer@163.com>
2023-04-13 16:07:27 +0800

    优化启动脚本

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 # Source docker-entrypoint.sh:
 if [ "$1" = 'freeswitch' ]; then
     # 如果是外部映射的配置文件目录，那么拷贝初始化数据到映射的目录
-    if [ ! -f "/usr/local/freeswitch/conf/freeswitch.xml" ]; then
-        mkdir -p /usr/local/freeswitch/conf
-        cp -varf /usr/local/freeswitch/.conf/* /usr/local/freeswitch/conf/
+    if [ ! -f "/usr/local/aiswitch/conf/freeswitch.xml" ]; then
+        #软连接目录
+        ln -sf /usr/local/freeswitch /usr/local/aiswitch
     fi
     
     if [ -d /docker-entrypoint.d ]; then
