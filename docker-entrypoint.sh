f5d74793c5fba7fc925636b018b652c9f46bfc81
JxAllen <icerleer@163.com>
2023-06-15 10:23:46 +0800

    修改shell启动脚本

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,30 +10,39 @@ if [ "$1" = 'freeswitch' ]; then
     fi
 
     # 创建默认的声音文件
-    if [ ! -e "/usr/local/freeswitch/sounds" ]; then
-        mkdir -p /usr/local/freeswitch/sounds
+	# 如果目录不存在
+	if [ ! -d "/usr/local/freeswitch/sounds" ]; then
+		mkdir -p /usr/local/freeswitch/sounds
         cp -arf /usr/local/freeswitch/.sounds/* /usr/local/freeswitch/sounds/
-    fi
+	# 如果目录存在，但是目录中不存在内容
+	elif [ ! -n "$(ls -A /usr/local/freeswitch/sounds)" ]; then
+        cp -arf /usr/local/freeswitch/.sounds/* /usr/local/freeswitch/sounds/
+	fi
+	
 
-    # 创建默认的脚本文件
-    if [ ! -e "/usr/local/freeswitch/scripts" ]; then
-        mkdir -p /usr/local/freeswitch/scripts
+    # 创建默认的脚本文件	
+	if [ ! -d "/usr/local/freeswitch/scripts" ]; then
+		mkdir -p /usr/local/freeswitch/scripts
         cp -arf /usr/local/freeswitch/.scripts/* /usr/local/freeswitch/scripts/
-    fi
+	elif [ ! -n "$(ls -A /usr/local/freeswitch/scripts)" ]; then
+        cp -arf /usr/local/freeswitch/.scripts/* /usr/local/freeswitch/scripts/
+	fi
 
     # 创建默认的ASR语法文件夹
-    if [ ! -e "/usr/local/freeswitch/grammar" ]; then
+	if [ ! -d "/usr/local/freeswitch/grammar" ]; then
         mkdir -p /usr/local/freeswitch/grammar
         cp -arf /usr/local/freeswitch/.grammar/* /usr/local/freeswitch/grammar/
+	elif [ ! -n "$(ls -A /usr/local/freeswitch/grammar)" ]; then
+		cp -arf /usr/local/freeswitch/.grammar/* /usr/local/freeswitch/grammar/
     fi
 
     # 创建默认的录音文件夹
-    if [ ! -e "/usr/local/freeswitch/recordings" ]; then
+    if [ ! -d "/usr/local/freeswitch/recordings" ]; then
         mkdir -p /usr/local/freeswitch/recordings
     fi
 
     # 创建默认的日志文件
-    if [ ! -e "/usr/local/freeswitch/log" ]; then
+    if [ ! -d "/usr/local/freeswitch/log" ]; then
         mkdir -p /usr/local/freeswitch/log
     fi
     
